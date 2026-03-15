25cfc49aeb83a6ae5e136e1cb226b7ad3a9722f7
Honza Pobořil <bobik@ibobik.cz>
2026-03-15 18:26:53 +0000

    Fixed accessability issues of the error page.

--- app/error.vue
+++ app/error.vue
@@ -8,22 +8,24 @@ const props = defineProps<{
 
 <template>
   <NuxtLayout name="wallpaper" bg-src="/app/not-found-wallpaper.png">
-    <div class="max-w-md flex flex-col items-center gap-10 text-white drop-shadow-lg">
+    <div class="max-w-md flex flex-col items-center gap-10 ">
       <h1 class="text-9xl font-bold text-primary/80">
         {{ error.status }}
       </h1>
 
-      <div class="text-3xl font-semibold">
+      <div class="text-4xl font-semibold text-white">
         <template v-if="error.status === 404">Špatná skládka</template>
         <template v-else>Nastala chyba</template>
       </div>
 
-      <template v-if="error.status === 404">
-        <p>Nebuť jako <ProseA href="https://forbes.cz/bitcoin-na-skladce-uz-brit-prestal-hledat-prakticka-lekce-jak-neprijit-o-miliardy/" target="_blank">James Howells</ProseA> který beznadějně hledá svůj harddisk s 8 000 bitcoiny na skládce v Newportu.</p>
-        <p>Zkontroluj jestli hledáš na správné adrese nebo se zkus prohrabat od začátku na hlavní stránce.</p>
-      </template>
+      <div class="">
+        <template v-if="error.status === 404">
+          <ProseP><span>Nebuť jako <ProseA href="https://forbes.cz/bitcoin-na-skladce-uz-brit-prestal-hledat-prakticka-lekce-jak-neprijit-o-miliardy/" target="_blank">James Howells</ProseA> který beznadějně hledá svůj harddisk s 8 000 bitcoiny na skládce v Newportu.</span></ProseP>
+          <ProseP><span>Zkontroluj jestli hledáš na správné adrese nebo se zkus prohrabat od začátku z hlavní stránky.</span></ProseP>
+        </template>
 
-      <p v-else>{{ error.message || 'Omlouváme se, něco se pokazilo.' }}</p>
+        <ProseP v-else><span>{{ error.message || 'Omlouváme se, něco se pokazilo.' }}</span></ProseP>
+      </div>
 
       <UButton
         size="lg"
@@ -33,3 +35,11 @@ const props = defineProps<{
     </div>
 </NuxtLayout>
 </template>
+
+<style lang="css" scoped>
+@reference "assets/css/main.css";
+
+span {
+  @apply px-2 py-1.5 bg-white box-decoration-clone rounded;
+}
+</style>
